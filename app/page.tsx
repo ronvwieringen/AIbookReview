6230091313e348fd1ab50bb2ee929ff85daba30c
ronvwieringen <ron.v.wieringen@gmail.com>
2025-06-07 22:26:11 +0200

    Updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -186,7 +186,7 @@ export default function Home() {
                   "Secure handling of your intellectual property",
                 ]}
                 bgColor="bg-[#2A4759]"
-                ctaText="Get AI Review"
+                ctaText="See authors benefits"
                 ctaLink="/authors"
               />
               <UserTypeValue
